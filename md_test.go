705263889c038daf3c62317c01cc23ba140dc640
Qs-F <qsf@de-liker.com>
2019-08-06 00:35:09 +0900

    Add md.go with test, for TDD

---
+++ md_test.go
@@ -0,0 +1,22 @@
+package yz
+
+import "testing"
+
+func TestParse(t *testing.T) {
+	errf := "input: %s, expect: %s, but have: %s\n"
+	tests := []struct {
+		Input  string
+		Expect string
+	}{
+		{
+			Input:  "# Title",
+			Expect: "<h1>Title</h1>",
+		},
+	}
+
+	for _, test := range tests {
+		if work := Parse(test.Input).HTML(); work != test.Expect {
+			t.Errorf(errf, test.Input, test.Expect, work)
+		}
+	}
+}
