bc831718c4b2ab7b307ddb352d7dd0c8249278c8
Qs-F <qsf@de-liker.com>
2019-08-06 00:55:33 +0900

    Add Parse and HTML funcs

--- md.go
+++ md.go
@@ -1,1 +1,31 @@
 package yz
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Type int
+
+const (
+	NoType Type = iota
+	HeadOne
+)
+
+type Node struct {
+	Type    Type
+	TagName string
+	Value   string
+}
+
+func Parse(src string) *Node {
+	return &Node{
+		Type:    HeadOne,
+		TagName: "h1",
+		Value:   strings.TrimLeft(src, "# "),
+	}
+}
+
+func (node *Node) HTML() string {
+	return fmt.Sprintf("<%s>%s</%s>", node.TagName, node.Value, node.TagName)
+}
