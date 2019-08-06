de1f3620af8dada3fee7ba0f79833e3edc52b574
Qs-F <qsf@de-liker.com>
2019-08-07 02:54:46 +0900

    Add new test for Parse

--- md_test.go
+++ md_test.go
@@ -2,8 +2,11 @@ package yz
 
 import "testing"
 
+const (
+	errf = "input: %s, expect: %s, but have: %s\n"
+)
+
 func TestParseAndHTML(t *testing.T) {
-	errf := "input: %s, expect: %s, but have: %s\n"
 	tests := []struct {
 		Input  string
 		Expect string
@@ -18,5 +21,22 @@ func TestParseAndHTML(t *testing.T) {
 		if work := Parse(test.Input).HTML(); work != test.Expect {
 			t.Errorf(errf, test.Input, test.Expect, work)
 		}
+	}
+}
+
+func TestParse(t *testing.T) {
+	head1 := &Type{
+		NET:     false,
+		TagName: h1,
+	}
+
+	tests := []struct {
+		Input  string
+		Expect *Node
+	}{
+		{
+			Input:  "# Title",
+			Expect: "<h1>Title</h1>",
+		},
 	}
 }
